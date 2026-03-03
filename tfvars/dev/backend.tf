7692c9c766131624a0c471b19e7c5cc6b77338ac
kirankm95 <153594511+kirankm95@users.noreply.github.com>
2026-03-03 21:41:11 +0530

    Delete tfvars directory

--- tfvars/dev/backend.tf
+++
@@ -1,4 +0,0 @@
-bucket = "kiranku-dev"
-key    = "multienv"
-region = "us-east-1"
-dynamodb_table = "kiranku-locking-dev"
--- tfvars/prod/backend.tf
+++
@@ -1,4 +0,0 @@
-bucket = "kiranku-prod"
-key    = "multienv"
-region = "us-east-1"
-dynamodb_table = "kiranku-locking-prod"
